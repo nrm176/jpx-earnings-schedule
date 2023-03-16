602e963f30e4a8c47b4b4d4c62327494e6bf0846
nrm176 <kojima37@gmail.com>
2023-03-16 22:50:49 +0900

    use Session instead of sessionmaker

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
-from sqlalchemy import *
-from sqlalchemy.orm import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 import os
 
 DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COBALT_URL').replace('postgres', 'postgresql')
-# setting for db
 engine = create_engine(DATABASE_URL, echo=False)
-session = sessionmaker(engine)
+session = Session(engine)
