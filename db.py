671e78d225b5495b9fcf485e6826f627deff5495
nrm176 <kojima37@gmail.com>
2023-03-16 22:43:02 +0900

    create engine

--- db.py
+++ db.py
@@ -4,13 +4,5 @@ import os
 
 DATABASE_URL = os.environ.get('HEROKU_POSTGRESQL_COBALT_URL').replace('postgres', 'postgresql')
 # setting for db
-ENGINE = create_engine(DATABASE_URL, echo=False)
-
-META = MetaData(bind=ENGINE)
-META.reflect()
-
-# create session
-session = scoped_session(
-    # Setting for ORM, auto commit is set to false. Do session.commit to commit
-    sessionmaker(ENGINE)
-)
+engine = create_engine(DATABASE_URL, echo=False)
+session = sessionmaker(engine)
